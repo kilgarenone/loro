b7b7dca36c87b53cfd64fc2f78dcd5bead9c86a4
Kheoh Yee Wei <kwei88@gmail.com>
2019-08-22 14:07:00 +0800

    resolve

--- schema/schema.js
+++ schema/schema.js
@@ -5,16 +5,35 @@ const { GraphQLDateTime } = require("graphql-iso-date");
 
 const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID } = graphql;
 
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: {
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    desc: { type: GraphQLString }
+  }
+});
+
 const TweetType = new GraphQLObjectType({
   name: "Tweet",
   fields: {
     id: { type: GraphQLID },
-    author: { type: GraphQLString },
-    tweet: { type: GraphQLString },
-    created_at: { type: GraphQLDateTime }
+    content: { type: GraphQLString },
+    // here we associate 'author' to a Tweet; we're walking from Tweet to associated author in the 'graph'
+    author: {
+      // when model's key is different(authorId) than graphql schema(author), you need 'resolve'.
+      type: AuthorType,
+      resolve(parentValue, args) {
+        // parentValue has the specific queried Tweet data
+        return axios
+          .get(`http://localhost:3001/authors/${parentValue.authorId}`)
+          .then(resp => resp.data);
+      }
+    }
   }
 });
 
+// RootQuery is your entry point to walking in the graph to get data you want along the way
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -22,16 +41,16 @@ const RootQuery = new GraphQLObjectType({
       type: TweetType,
       args: { id: { type: GraphQLID } },
       resolve(parentValue, args) {
-        // return axios
-        //   .get(`http://localhost:3001/tweet/${args.id}`)
-        //   .then(resp => resp.data);
-        const query = `SELECT * FROM tweet WHERE id=$1`;
-        const values = [args.id];
+        return axios
+          .get(`http://localhost:3001/tweets/${args.id}`)
+          .then(resp => resp.data);
+        // const query = `SELECT * FROM tweet WHERE id=$1`;
+        // const values = [args.id];
 
-        return db
-          .query(query, values)
-          .then(res => res.rows[0]) // .rows is by postgres! CAN NOT JUST RETURN 'res' !!! Graphql won't get it!!!
-          .catch(err => err);
+        // return db
+        //   .query(query, values)
+        //   .then(res => res.rows[0]) // .rows is by postgres! CAN NOT JUST RETURN 'res' !!! Graphql won't get it!!!
+        //   .catch(err => err);
       }
     }
   }
